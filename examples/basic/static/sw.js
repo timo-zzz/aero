bfe0b12286fc99250f91c7cdcbeb781eadd58f27
Ryan Wilson <inbox@ryanwilson.space>
2022-02-19 18:46:24 -0500

    ultravilot kinda �

--- examples/basic/static/sw.js
+++ examples/basic/static/sw.js
@@ -17,15 +17,28 @@ var ctx = {
 }
 
 // Set the server ctx.
-self.addEventListener('message', event => {
-	console.log(event);
-	ctx = event.data;
-});
+//self.addEventListener('message', event => ctx = event.data);
 
 // Oh no I will have to fix prefetch
 
 self.origin = '';
 self.gel = `
+	Object.defineProperty(document, 'cookie', {
+		get() {
+			//return document.cookie;
+		},
+		set(value) {
+			document.cookie = value;
+		}
+	});
+	Object.defineProperty(document, 'domain', {
+		get() {
+		  	return new URL(ctx.origin).hostname;
+		},
+		set(value) {
+		  	//return value;
+		}
+	});
 	WebSocket = new Proxy(WebSocket, {
 		construct(target, args) {
 			const protocol = args[0].split('://')[0] + '://';
@@ -34,9 +47,15 @@ self.gel = `
 			return Reflect.construct(target, args);
 		}
 	});
+	Worker = new Proxy(Worker, {
+		construct(target, args) {
+		  alert(args[0]);
+		  //return Reflect.construct(target, args);
+		}
+	});
 	// Discord uses this
 	var historyState = {
-		apply (target, that, args) {
+		apply(target, that, args) {
 			if (args[2])
 				args[2] = ctx.origin + args[2];
 			alert(args[2]);
@@ -59,14 +78,19 @@ self.gel = `
 			return Reflect.apply(target, that, args);
 		}
 	});
-
 	var fakeLocation = new Proxy(location, {
 		get(target, prop) {
-			console.log(\`get \${prop}\`);
-			return Reflect.get(target, prop);
+			console.log(prop);	
+			console.log(ctx.origin);
+			console.log(new URL(ctx.origin)[prop]);
+			if (typeof target[prop] === 'function') {
+				return {
+					assign: () => null
+				}[prop];
+			}
+			else return new URL(ctx.origin)[prop];
 		},
 		set(target, prop, value) {
-			console.log(\`set \${prop} to \${value}\`);
 			return Reflect.set(target, that, args);
 		}
 	})
@@ -96,119 +120,120 @@ self.addEventListener('fetch', event => {
 					<!-- In case csp isn't set to unsafe-inline -->
 					<!--<meta http-equiv="Content-Security-Policy" content="script-src 'nonce-${scriptNonce}'"-->
 					<meta charset="utf-8">
-				</head>
-				<script nonce=${scriptNonce}>
-					const ctx = ${JSON.stringify(ctx)};
-					ctx.origin = '${origin}';
-					
-					function rewriteUrl(url) {
-						if (url.startsWith('about:') || url.startsWith('data:') || url.startsWith('javascript:') || url.startsWith('mailto:'))
+					<script nonce=${scriptNonce}>
+						const ctx = ${JSON.stringify(ctx)};
+						ctx.origin = '${origin}';
+						
+						function rewriteUrl(url) {
+							if (url.startsWith('about:') || url.startsWith('data:') || url.startsWith('javascript:') || url.startsWith('mailto:'))
+								return url;
+							else if (url.startsWith(location.origin)) {
+								const raw = window.location.pathname.split(ctx.http.prefix)[1];
+
+								const origin = new URL(raw).origin;
+								if (raw.startsWith(origin)) {
+									return ctx.http.prefix + origin + url.split(location.origin)[1].split(ctx.http.prefix)[0];
+								}
+
+								const protocolSplit = url.split(location.origin)[1].split('https://');
+
+								return ctx.http.prefix + window.location.pathname.split(ctx.http.prefix)[1] + '/' + protocolSplit[protocolSplit.length - 1];
+							} else
+								return ctx.http.prefix + url;
 							return url;
-						else if (url.startsWith(location.origin)) {
-							const raw = window.location.pathname.split(ctx.http.prefix)[1];
+						}
 
-							const origin = new URL(raw).origin;
-							if (raw.startsWith(origin)) {
-								return ctx.http.prefix + origin + url.split(location.origin)[1].split(ctx.http.prefix)[0];
-							}
+						let firstScript = true;
+						new MutationObserver((mutations, observer) => {
+							//console.log(mutations);	
+							for (let mutation of mutations)
+								for (let node of mutation.addedNodes) {
+									let stack = [node];
 
-							const protocolSplit = url.split(location.origin)[1].split('https://');
+									while (node = stack.pop()) {
+										if (node instanceof HTMLScriptElement) {
+											if (!firstScript) {
+												const script = document.createElement('script');
 
-							return ctx.http.prefix + window.location.pathname.split(ctx.http.prefix)[1] + '/' + protocolSplit[protocolSplit.length - 1];
-						} else
-							return ctx.http.prefix + url;
-						return url;
-					}
+												// Copy properties to the new script
+												if (node.async)
+													script.async = node.async;
+												if (node.crossorigin)
+													script.crossorigin = node.crossorigin;
+												if (node.defer)
+													script.defer = node.defer;
+												if (node.id)
+													script.id = node.id;
+												if (node.integrity)
+													script._integrity = node.integrity;
+												if (node.nonce)
+													script.nonce = node.nonce;
+												if (node.referrerpolicy)
+													script.referrerpolicy = node.referrerpolicy;
+												if (node.src)
+													script.src = node.src;
+												if (node.type)
+													script.type = node.type;
 
-					let firstScript = true;
-					new MutationObserver((mutations, observer) => {	
-						for (let mutation of mutations)
-							for (let node of mutation.addedNodes) {
-								let stack = [node];
+												// Scope
+												if (node.text !== '' && node.type !== 'application/json')
+													script.innerHTML = node.text.replace(/location/g, '_location');
 
-								while (node = stack.pop()) {
-									if (node instanceof HTMLScriptElement) {
-										if (!firstScript) {
-											const script = document.createElement('script');
+												// Insert rewritten script
+												node.after(script);
 
-											// Copy properties to the new script
-											if (node.async)
-												script.async = node.async;
-											if (node.crossorigin)
-												script.crossorigin = node.crossorigin;
-											if (node.defer)
-												script.defer = node.defer;
-											if (node.id)
-												script.id = node.id;
-											if (node.integrity)
-												script._integrity = node.integrity;
-											if (node.nonce)
-												script.nonce = node.nonce;
-											if (node.referrerpolicy)
-												script.referrerpolicy = node.referrerpolicy;
-											if (node.src)
-												script.src = node.src;
-											if (node.type)
-												script.type = node.type;
+												// Clean up old script
+												node.remove();
 
-											// Scope
-											if (node.text !== '' && node.type !== 'application/json')
-												script.innerHTML = node.text.replace(/location/g, '_location');
+												// Don't record this recent mutation
+												observer.takeRecords();
 
-											// Insert rewritten script
-											node.after(script);
+												continue;
+											} else firstScript = false;
+										} else if ((node instanceof HTMLIFrameElement || 'HTMLPortalElement' in window && node instanceof HTMLPortalElement) && node.src) {
+											const rewrittenUrl = rewriteUrl(node.src);
 
-											// Clean up old script
-											node.remove();
+											console.log(\`%csrc%c \${node.src} %c->%c \${rewrittenUrl}\`, 'color: dodgerBlue', '', 'color: mediumPurple', '');
 
-											// Don't record this recent mutation
-											observer.takeRecords();
+											node.href = rewrittenUrl;
+										} else if (node.href && !(node instanceof HTMLLinkElement)) {
+											const rewrittenUrl = rewriteUrl(node.href);
+											
+											console.log(\`%chref%c \${node.href} %c->%c \${rewrittenUrl}\`, 'color: dodgerBlue', '', 'color: mediumPurple', '');
+											
+											node.href = rewrittenUrl;	
+										} else if (node.action) {
+											const rewrittenUrl = rewriteUrl(node.action);
 
-											continue;
-										} else firstScript = false;
-									} else if (node instanceof HTMLIFrameElement || 'HTMLPortalElement' in window && node instanceof HTMLPortalElement && node.src) {
-										const rewrittenUrl = rewriteUrl(node.src);
+											console.log(\`%caction%c \${node.action} %c->%c \${rewrittenUrl}\`, 'color: dodgerBlue', '', 'color: mediumPurple', '');
 
-										console.log(\`%csrc%c \${node.src} %c->%c \${rewrittenUrl}\`, 'color: dodgerBlue', '', 'color: mediumPurple', '');
+											node.action = rewrittenUrl;
+										} else if (node instanceof HTMLMetaElement && node.httpEquiv === 'refresh' && node.content) {
+											const rewrittenUrl = rewriteUrl(node.content);
 
-										node.href = rewrittenUrl;
-									} else if (node.href && !(node instanceof HTMLLinkElement)) {
-										const rewrittenUrl = rewriteUrl(node.href);
-										
-										console.log(\`%chref%c \${node.href} %c->%c \${rewrittenUrl}\`, 'color: dodgerBlue', '', 'color: mediumPurple', '');
-										
-										node.href = rewrittenUrl;	
-									} else if (node.action) {
-										const rewrittenUrl = rewriteUrl(node.action);
+											console.log(\`%refresh%c \${node.content} %c->%c \${rewrittenUrl}\`, 'color: dodgerBlue', '', 'color: mediumPurple', '');
 
-										console.log(\`%caction%c \${node.action} %c->%c \${rewrittenUrl}\`, 'color: dodgerBlue', '', 'color: mediumPurple', '');
-
-										node.action = rewrittenUrl;
-									} else if (node instanceof HTMLMetaElement && node.httpEquiv === 'refresh' && node.content) {
-										const rewrittenUrl = rewriteUrl(node.content);
-
-										console.log(\`%refresh%c \${node.content} %c->%c \${rewrittenUrl}\`, 'color: dodgerBlue', '', 'color: mediumPurple', '');
-
-										node.content = rewrittenUrl;
+											node.content = rewrittenUrl;
+										}
 									}
 								}
-							}
-					}).observe(document, {
-						childList: true,
-						subtree: true
-					});
-					
-					// Update the url hash.
-					//addEventListener('hashchange', event => ctx.url = location.hash);
-					
-					// Clear history.
-					history.replaceState({}, '');
-					
-					// Don't set the history.
-					addEventListener('popstate', event => event.preventDefault());
-					
-					${gel}
-				</script>
+						}).observe(document, {
+							childList: true,
+							subtree: true
+						});
+						
+						// Update the url hash.
+						//addEventListener('hashchange', event => ctx.origin = location.hash);
+						
+						// Clear history.
+						history.replaceState({}, '');
+						
+						// Don't set the history.
+						addEventListener('popstate', event => event.preventDefault());
+						
+						${gel}
+					</script>
+				</head>
 				${text} 
 			` : response.body, {
 				status: response.status,
@@ -229,6 +254,8 @@ self.addEventListener('fetch', event => {
 			else {
 				const prefixSplit = originSplit[1].split(ctx.http.prefix);
 
+				//console.log(prefixSplit);
+
 				// If the url is already valid then don't do anything
 				if (prefixSplit.length === 2 && prefixSplit[1].startsWith(url)) {
 					url += prefixSplit[1];
@@ -236,7 +263,9 @@ self.addEventListener('fetch', event => {
 				else {
 					const protocolSplit = prefixSplit[prefixSplit.length - 1].split('https:/');
 					
-					url += origin + protocolSplit[protocolSplit.length - 1];
+					var path = protocolSplit[protocolSplit.length - 1].split('/' + new URL(origin).hostname)[1];
+
+					url += origin + path;
 				}
 			}
 		}
@@ -267,7 +296,9 @@ self.addEventListener('fetch', event => {
 			headers: {
 				...event.request.headers,
 				_Referer: origin
-			}
+			},
+			method: event.request.method,
+			mode: event.request.mode
 		});
 
 		// Easy way to handle streams
@@ -279,13 +310,6 @@ self.addEventListener('fetch', event => {
 		let text = await response.text();
 
 		if (event.request.destination === 'script') {
-			/*
-			text = `
-				${gel}
-
-				${text.replace(/location/g, '_location')}
-			`;
-			*/
 			text = text.replace(/location/g, '_location');
 		}
 
@@ -296,23 +320,7 @@ self.addEventListener('fetch', event => {
 		return new Response(text, {
 			status: response.status,
 			statusText: response.statusText,
-			// Fonts will be fixed whenever content-length is removed
-			// I am currently trying to get https://coolmath.com and https://ridgethread.com/ to work
 			headers: headers
 		});
 	}());
-});
-
-/*
-Only supports chromium with the flag enable-experimental-cookie-features and a secure context
-https://wicg.github.io/cookie-store/#typedefdef-cookielist
-*/
-self.addEventListener('cookiechange', event => {
-	for (const cookie of event.changed) {
-		console.log('Cookie changed', cookie);
-	}
-
-	for (const cookie of event.deleted) {
-		console.log('Deleted a cookie', cookie);
-	}
 });
